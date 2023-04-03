09c955e88fd5a1704c3c98f9a9a5ef4c3f5fa1c9
MADHUR-GUPTA <madhurmawai@gmail.com>
2023-04-03 10:11:27 +0530

    Jan 2023 all projects added

---
+++ 1901/form.php
@@ -0,0 +1,11 @@
+
+
+<html>
+<body>
+  <form action="form1.php" method="GET">
+    Name: <input type="text" name="fname" />
+    Weight:<input type="text" name="weight" />
+           <input type="submit" />
+  </form>
+</body>
+</html>
---
+++ 1901/form1.php
@@ -0,0 +1,5 @@
+<?php
+
+$e = ("this is ".$_GET["fname"]);
+echo($e);
+?>
